87848851b7bf1f19354ab8ef6ae8e5f2bd228081
abolfazlhadinejad <161231630+abolfazlhadinejad@users.noreply.github.com>
2024-03-04 11:53:22 +0330

    normalizingName function

--- Base Structure/src/main/java/Advanced.java
+++ Base Structure/src/main/java/Advanced.java
@@ -4,28 +4,24 @@ public class Advanced {
 
     /**
      * Goal : Changing a Sentence Content
-
+     * <p>
      * In this function, you have a sentence, a word & a newWord as Entry
      * You have to search the sentence to find the word that you were given as input and change it with the newWord
-
      */
-    public String wordCensor(String sentence, String word, String newWord)
-    {
+    public String wordCensor(String sentence, String word, String newWord) {
         String[] arraySentence = sentence.split(" ");
         int numberOfWords = arraySentence.length;
 
-        for (int i=0 ; i < numberOfWords ; i++)
-        {
-            if (arraySentence[i].equals(word))
-            {
+        for (int i = 0; i < numberOfWords; i++) {
+            if (arraySentence[i].equals(word)) {
                 arraySentence[i] = newWord;
             }
         }
 
         String finalWord = "";
-        for (int j = 0 ; j < numberOfWords ; j++)
+        for (int j = 0; j < numberOfWords; j++)
         {
-            if (j == numberOfWords-1)
+            if (j == numberOfWords - 1)
             {
                 finalWord = finalWord + arraySentence[j];
             }
@@ -41,17 +37,79 @@ public class Advanced {
 
     /**
      * In this function You have a firstName and a lastName as Entry and you have to normalize them as a fullName
+     *
      * @param firstName is a first name with irregular letters (example : hARry)
-     * @param lastName is a last name with irregular letters (example : pOtTeR)
+     * @param lastName  is a last name with irregular letters (example : pOtTeR)
      * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
      */
     public String normalizingName(String firstName, String lastName)
     {
-        return null;
+        char[] arrayFirstName = firstName.toCharArray();
+        char[] arrayLastName = lastName.toCharArray();
+
+        int lengthOfFirstName = arrayFirstName.length;
+        int lengthOfLastName = arrayLastName.length;
+        /*
+        اگر بازه کد اسکی کاراکتر بین 65 تا 90 بود یعنی حروف بزرگ است و باید به حروف کوچک تبدیل شود پس بعلاوه 32 میکنیم
+        اگر در بازه 97  تا 122 بود یعنی حروف کوچک است و باید به حروف بزرگ تبدیل شود پس منهای 32 میکنی
+         */
+        if ( arrayFirstName[0] >= 97 && arrayFirstName[0] <= 122 )
+        {
+            arrayFirstName[0] = (char) (arrayFirstName[0] - 32);
+            //arrayFirstName[0] = Character.toUpperCase(arrayFirstName[0]);
+        }
+
+        if ( arrayLastName[0] >= 97 && arrayLastName[0] <= 122 )
+        {
+           //arrayLastName[0] = (char) (arrayLastName[0] - 32);
+           arrayLastName[0] = Character.toUpperCase(arrayLastName[0]);
+        }
+
+        for (int i = 1 ; i < lengthOfFirstName ; i++)
+        {
+            if (arrayFirstName[i] >= 65 && arrayFirstName[i] <= 90)
+            {
+                arrayFirstName[i] = (char) (arrayFirstName[i] + 32);
+            }
+        }
+
+        for (int i = 1 ; i < lengthOfLastName ; i++)
+        {
+            if (arrayLastName[i] >= 65 && arrayLastName[i] <= 90)
+            {
+                arrayLastName[i] = Character.toLowerCase(arrayLastName[i]);
+                //arrayLastName[i] = (char) (arrayLastName[i] + 32);
+            }
+        }
+
+
+
+        String finalWord = "";
+        for (int j = 0; j < lengthOfFirstName; j++)
+        {
+                finalWord = finalWord + arrayFirstName[j];
+        }
+
+        if (lastName != " ")
+        {
+            finalWord = finalWord + " ";
+        }
+
+        if (lastName != " ")
+        {
+            for (int j = 0; j < lengthOfLastName; j++)
+            {
+                finalWord = finalWord + arrayLastName[j];
+
+            }
+        }
+
+        return finalWord;
     }
 
     /**
      * Removing repeated letter in a word
+     *
      * @param word This input could have Consecutive repeated letters or not
      * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
      */
@@ -59,17 +117,14 @@ public class Advanced {
     {
         String[] arraySentence = word.split("");
         int lengthOfArray = arraySentence.length;
-        for (int i = 1 ; i < lengthOfArray ; i++)
-        {
-            if (Objects.equals(arraySentence[i],arraySentence[i-1]))
-            {
-                arraySentence[i-1] = "";
+        for (int i = 1; i < lengthOfArray; i++) {
+            if (Objects.equals(arraySentence[i], arraySentence[i - 1])) {
+                arraySentence[i - 1] = "";
             }
         }
 
         String finalSentence = "";
-        for (int i = 0 ; i < lengthOfArray ; i++)
-        {
+        for (int i = 0; i < lengthOfArray; i++) {
             finalSentence += arraySentence[i];
         }
         return finalSentence;
